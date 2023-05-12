544367503eea91a7fdfbc870d543b518cc854393
Randall Dang <82141536+RandallDang-Citi@users.noreply.github.com>
2023-05-12 11:53:26 +0800

    Add files via upload

---
+++ Yuan Guo/test/src/main/java/test/MappingBo.java
@@ -0,0 +1,24 @@
+package test;
+
+
+public class MappingBo {
+
+    private String json;
+    private String xml;
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+}
